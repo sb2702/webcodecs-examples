69b2ef29c91fa2cb381341cb8727c9745a702e41
Sam Bhattacharyya <sambhattacharyya@gmail.com>
2026-01-05 17:54:51 -0600

    extract chunks

--- src/transcoding/transcode-promise.ts
+++ src/transcoding/transcode-promise.ts
@@ -1,3 +1,6 @@
+
+import {MP4Demuxer} from 'webcodecs-utils'
+
 /**
  * Transcoding implementation - Promise pattern
  * (How free.upscaler does it)
@@ -6,6 +9,21 @@
 export async function transcodePromise(file: File): Promise<Blob> {
   // TODO: Implement promise pattern
   // - Explanation of this approach
+
+
+  const demuxer = new MP4Demuxer(file);
+
+  await demuxer.load();
+
+
+  const trackData = demuxer.getTracks();
+
+  const videoDecoderConfig = demuxer.getVideoDecoderConfig();
+
+  const videoChunks = await demuxer.extractSegment('video', 0, trackData.duration );
+
+
+
 
   return new Blob([], { type: "video/mp4" });
 }
