0e6114050139bc52baf21d83810c70e1a5e8edf9
Sam Bhattacharyya <sambhattacharyya@gmail.com>
2026-01-06 15:02:03 -0600

    Clean up

--- src/transcoding/index.ts
+++ src/transcoding/index.ts
@@ -1,5 +1,4 @@
 export { transcodeFile } from './transcoder';
 export type { TranscodeMethod } from './transcoder';
 export { transcodePromise } from './transcode-promise';
-export { transcodeWaterfall } from './transcode-waterfall';
 export { transcodePipeline } from './transcode-pipeline';
