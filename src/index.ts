d28c053c9f05a7867d9aa16e3575e71064d4f4b3
Sam Bhattacharyya <sambhattacharyya@gmail.com>
2026-01-06 15:11:00 -0600

    Move to index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 // Main entry point for webcodecs-examples package
 export { WebCodecsPlayer } from './player/index';
+
+export { transcodeFile } from './transcoding/transcoder';
+export type { TranscodeMethod } from './transcoding/transcoder';
+export { transcodePromise } from './transcoding/transcode-promise';
+export { transcodePipeline } from './transcoding/transcode-pipeline';
