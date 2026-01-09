1f305947f317295d03a192cff8c6966bf4699ed8
Sam Bhattacharyya <sambhattacharyya@gmail.com>
2026-01-09 11:14:40 -0600

    bump

--- demos/webcam-recording/demo.ts
+++ demos/webcam-recording/demo.ts
@@ -1,6 +1,5 @@
 import { getWebcam } from '../../src/webcam-recording/webcam';
-import { TrackProcessor } from '../../src/webcam-recording/track-processor';
-import { GPUFrameRenderer } from 'webcodecs-utils';
+import { GPUFrameRenderer, MediaStreamTrackProcessor } from 'webcodecs-utils';
 
 document.getElementById('startBtn')?.addEventListener('click', async () => {
   const { videoTrack, stream } = await getWebcam();
@@ -9,15 +8,10 @@ document.getElementById('startBtn')?.addEventListener('click', async () => {
 
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const renderer = new GPUFrameRenderer(canvas);
-  
   await renderer.init();
 
-
-  
-
-
-  const frameStream = TrackProcessor(videoTrack);
-  const reader = frameStream.getReader();
+  const processor = new MediaStreamTrackProcessor({ track: videoTrack });
+  const reader = processor.readable.getReader();
 
   while (true) {
     const { done, value: frame } = await reader.read();
