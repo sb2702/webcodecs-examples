947c6f9fbdc714ae0d63d3072e8aa2057aec2199
Sam Bhattacharyya <sambhattacharyya@gmail.com>
2026-01-10 12:07:50 -0600

    Stuff

--- src/moq/index.ts
+++ src/moq/index.ts
@@ -1,3 +1,4 @@
 export { MoqPublisher } from './moq-publisher';
 export { MoqSubscriber } from './moq-subscriber';
+export { AudioPlayer } from './audio-player';
 export type { MoqFrame } from './moq-subscriber';
